00a6fdf8349093c66f960a14a4ba651a92787a5e
David Batista <dbatista.t@gmail.com>
2020-03-23 20:33:54 -0400

    Fixed add product and added updated product

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,33 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { Nack, RabbitRPC } from '@nestjs-plus/rabbitmq';
+import { AmqpConnection, Nack, RabbitRPC } from '@nestjs-plus/rabbitmq';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Event } from './entities/event.entity';
-import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { Repository } from 'typeorm';
 import { AddProduct } from './interface/add-product.interface';
+import { UpdateProduct } from './interface/update-product.interface';
 
 @Injectable()
 export class AppService {
   constructor(
-    @InjectRepository(Product) private repository: Repository<Product>,
+    @InjectRepository(Product) private productRepository: Repository<Product>,
     @InjectRepository(Event) private eventRepository: Repository<Event>,
+    private readonly amqpConnection: AmqpConnection,
   ) {
-  }
-
-  async findAll(): Promise<Product[]> {
-    return await this.repository.find();
-  }
-
-  async create(entity: Product): Promise<Product> {
-    return await this.repository.save(entity);
-  }
-
-  async update(entity: Product): Promise<UpdateResult> {
-    return await this.repository.update(entity.id, entity);
-  }
-
-  async delete(id): Promise<DeleteResult> {
-    return await this.repository.delete(id);
   }
 
   @RabbitRPC({
@@ -35,26 +21,64 @@ export class AppService {
     routingKey: 'add-product',
     queue: 'kardex-sql',
   })
-  public async addProduct(product: AddProduct) {
-    console.log('RABBIT RESPONSE', product);
+  public async create(product: AddProduct) {
+    console.log('ADD PRODUCT', product);
     try {
       const newProduct = new Product();
       newProduct.price = product.price;
       newProduct.quantity = product.qty;
       newProduct.productCode = product.productCode;
       newProduct.registerDate = product.registerDate;
-      const res = await this.repository.save(newProduct);
-
-      const newEvent = new Event();
-      newEvent.type = EnumEventType.CREATED;
-      newEvent.object = JSON.stringify(newProduct);
-      await this.eventRepository.save(newEvent);
-
-      return res;
+      const result = await this.productRepository.save(newProduct);
+      await this.createEvent(result, EnumEventType.CREATED);
+      return result;
     } catch (e) {
-      console.log('ERROR:', e);
+      return new Nack(false);
+    }
+  }
+
+  @RabbitRPC({
+    exchange: 'kardex',
+    routingKey: 'update-product',
+    queue: 'kardex-sql',
+  })
+  async update({ id, qty, price }: UpdateProduct) {
+    console.log('UPDATE PRODUCT', { id, qty, price });
+    try {
+      const entity = await this.productRepository.findOne({ id });
+      if (!entity) {
+        return;
+      }
+      entity.quantity = qty;
+      entity.price = price;
+      const result = await this.productRepository.update(entity.id, entity);
+      await this.createEvent(result, EnumEventType.UPDATED);
+      const qtyResult = entity.quantity - qty;
+      this.updateQtyView({ qty: qtyResult, productCode: entity.productCode });
+      this.updatePriceView({ productCode: entity.productCode, price });
+      return result;
+    } catch (e) {
+      console.log('ERROR: ', e);
       return new Nack(true);
     }
   }
 
+  private async createEvent(product, event: EnumEventType) {
+    const newEvent = new Event();
+    newEvent.type = EnumEventType.CREATED;
+    newEvent.object = JSON.stringify(product);
+    await this.eventRepository.save(newEvent);
+  }
+
+  private updateQtyView({ productCode, qty }) {
+    if (qty >= 0) {
+      this.amqpConnection.publish('kardex', 'substract-qty-product', { productCode, qty: Math.abs(qty) });
+    } else {
+      this.amqpConnection.publish('kardex', 'add-qty-product', { productCode, qty: Math.abs(qty) });
+    }
+  }
+
+  private updatePriceView({ productCode, price }) {
+    this.amqpConnection.publish('kardex', 'change-price-product', { productCode, price });
+  }
 }
---
+++ src/interface/update-product.interface.ts
@@ -0,0 +1,5 @@
+export interface UpdateProduct {
+  id: number;
+  qty: number;
+  price: number;
+}
